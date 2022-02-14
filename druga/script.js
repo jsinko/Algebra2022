067934656fe3f66a284e64456d5f59d3a9b683d8
Jelena Šinko Jagačić <56638319+jsinko@users.noreply.github.com>
2022-02-14 12:10:25 +0100

    animacija
    dodana animacija

---
+++ druga/script.js
@@ -0,0 +1,21 @@
+
+  anime({
+  targets:'.animation2',
+  left: '500px',
+  backgroundColor: '#FFF',
+  borderRadius: ['0%', '50%'],
+  easing: 'easeInOutQuad',
+  delay: 3000
+  
+});
+
+anime({
+  targets:'.animation1',
+  width: '100%',
+  easing: 'easeInOutQuad',
+  direction: 'alternate',
+  loop: true
+  
+
+});
+  
